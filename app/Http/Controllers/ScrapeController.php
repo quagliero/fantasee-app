2218f5be597193181510afec10ac64d92d157ef4
Tobias <tobiaswhd@gmail.com>
2015-03-31 22:50:53 +0100

    More scrapes moved over from the JS proto. I miss async.

--- app/Http/Controllers/ScrapeController.php
+++ app/Http/Controllers/ScrapeController.php
@@ -8,14 +8,96 @@ use Goutte\Client;
 
 class ScrapeController extends Controller {
 
-	public function index() {
+	public function index(Request $request) {
 		$client = new Client();
+		$leagueId = $request->route('leagueId');
 
-		$crawler = $client->request('GET', 'http://fantasy.nfl.com/league/874089/history');
+		$crawler = $client->request('GET', 'http://fantasy.nfl.com/league/' . $leagueId . '/history');
 
 		// Get the latest post in this category and display the titles
 		$crawler->filter('#historySeasonNav .st-menu a[href]')->each(function ($node) {
-			print $node->text()."\n";
+			print $node->text()."<br>";
 		});
+
+		print '<br>';
+
+		// display season champs
+		$seasons = [];
+
+		$crawler->filter('#leagueHistoryAlmanac [class*="history-champ"]')->each(function ($node) {
+			$year = $node->filter('.historySeason')->text();
+			$team = $node->filter('.historyTeam .teamName')->text();
+
+			print $year . ' Champion: ' . $team . '<br>';
+		});
+
+		print '<br>';
+
+		// display season best week score
+		$crawler->filter('#leagueHistoryAlmanac [class*="history-btw"]')->each(function ($node) {
+			$row = $node;
+			$year = $row->filter('.historySeason')->text();
+			$week = $row->filter('.historyWeek')->text();
+			$team = $row->filter('.historyTeam .teamName')->text();
+			$points = $row->filter('.historyPts')->text();
+
+			print $year . ' Weekly Points Winner: ' . $team . ' with ' . $points . ' points in week ' . $week . '<br>';
+		});
+
+		print '<br>';
+
+		// display best player week score
+		$crawler->filter('#leagueHistoryAlmanac [class*="history-bpw"]')->each(function ($node) {
+			$row = $node;
+			$year = $row->filter('.historySeason')->text();
+			$week = $row->filter('.historyWeek')->text();
+			$team = $row->filter('.historyTeam .teamName')->text();
+			$player = $row->filter('.playerNameAndInfo .playerName')->text();
+			$posTeam = $row->filter('.playerNameAndInfo em')->text();
+			$points = $row->filter('.historyPts')->text();
+			print $year . ' Weekly Player Points Winner: ' . $team . ' with ' . $player . ' (' . $posTeam . ') with ' . $points . ' points in week ' . $week . '<br>';
+		});
+
+		print '<br>';
+
+		// display team season high points
+		$crawler->filter('#leagueHistoryAlmanac [class*="history-bts"]')->each(function ($node) {
+			$row = $node;
+			$year = $row->filter('.historySeason')->text();
+			$team = $row->filter('.historyTeam .teamName')->text();
+			$points = $row->filter('.historyPts')->text();
+
+			print $year . ' Season Points Winner: ' . $team . ' with ' . $points . ' points<br>';
+		});
+
+		print '<br>';
+
+		// season standings
+		$seasons = [2012, 2013, 2014];
+		foreach ($seasons as $season) {
+			print '<h2>' . $season . '</h2>';
+			$crawler = $client->request('GET', 'http://fantasy.nfl.com/league/' . $leagueId . '/history/' . $season . '/standings');
+			$crawler->filter('#finalStandings #championResults .results li')->each(function ($node, $i) {
+				$pos = $i += 1;
+				$team = $node->filter('.teamName')->text();
+				print $pos . ' : ' . $team . '<br>';
+			});
+		}
+
+		foreach ($seasons as $season) {
+			$crawler = $client->request('GET', 'http://fantasy.nfl.com/league/' . $leagueId . '/history/' . $season . '/draftresults?draftResultsDetail=0&draftResultsTab=round&draftResultsType=results');
+			print '<h2>' . $season . ' Draft</h2>';
+			$crawler->filter('#leagueDraftResults #leagueDraftResultsResults .results .wrap > ul')->each(function ($round, $i) {
+				print '<h3>Round ' . ($i + 1) . '</h3>';
+				$round->children('li')->each(function ($pick) {
+          $num = $pick->filter('.count')->text();
+          $player = $pick->filter('.playerName')->text();
+          $team = $pick->filter('.teamName')->text();
+          print 'Pick ' . $num . ': ' . $player . ' TO ' . $team . '<br>';
+        });
+			});
+		}
+
+
 	}
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,7 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
-Route::get('scrape', 'ScrapeController@index');
+Route::get('scrape/{leagueId}', 'ScrapeController@index');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
