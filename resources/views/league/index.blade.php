7b3d8a9265e32d6280d9f7ea4939461211c4c9cc
Tobias <tobiaswhd@gmail.com>
2015-04-09 22:09:35 +0100

    Quick tidy up of leagues home

--- resources/views/league/index.blade.php
+++ resources/views/league/index.blade.php
@@ -5,11 +5,36 @@
 @if (Auth::check())
   <p>{!! link_to_route('league_create', 'Add new league', null, ['class="btn btn-primary"'])!!}</p>
 @else
-  <p><a href="{{ url('/auth/login') }}">Login</a> or <a href="{{ url('/auth/register') }}">Register</a> to add your league.</p>
+  <p class="alert alert-info"><i class="glyphicon glyphicon-info-sign"></i>
+    <a href="{{ url('/auth/login') }}">Login</a> or <a href="{{ url('/auth/register') }}">Register</a> to add your league.</p>
 @endif
-<ul>
-@foreach ($leagues as $league)
-<li>{!! link_to_route('league_path', $league->name, [$league->slug]) !!}</li>
-@endforeach
-</ul>
-@endsection
+
+<div class="row">
+  @foreach ($leagues as $league)
+  <div class="col-sm-4 col-xs-6">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">{{ $league->name }}</h3>
+      </div>
+        <ul class="list-group">
+          <li class="list-group-item">
+            <i class="glyphicon glyphicon-calendar"></i> <strong>Seasons</strong>
+            <span class="badge">{{ $league->seasons->count() }}</span>
+          </li>
+          <li class="list-group-item">
+            <i class="glyphicon glyphicon-user"></i> <strong>Managers</strong>
+            <span class="badge">{{ $league->managers->count() }}</span>
+          </li>
+          <li class="list-group-item">
+            <i class="glyphicon glyphicon-king"></i> <strong>Champions</strong>
+            <span class="badge">3</span>
+          </li>
+          <li class="list-group-item">
+            {!! link_to_route('league_path', 'Explore league', [$league->slug], ['class' => 'btn btn-primary btn-block']) !!}
+          </li>
+        </ul>
+      </div>
+  </div>
+  @endforeach
+</div>
+@stop
