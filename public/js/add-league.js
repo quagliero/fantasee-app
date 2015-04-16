f41ea4afb5bfae96e75bbaf86ed7ee033c10a56e
Tobias <tobiaswhd@gmail.com>
2015-04-16 22:03:12 +0100

    No need for jQuery there

--- public/js/add-league.js
+++ public/js/add-league.js
@@ -1,12 +1,12 @@
-(function ($) {
+(function () {
   'use strict';
 
-  var $name = $('#name');
-  var $slug = $('#slug');
+  var name = document.getElementById('name');
+  var slug = document.getElementById('slug');
 
-  $name.on('keyup', function () {
-    var slug = $(this).val().replace(/\s+/g, '-').toLowerCase();
-    $slug.val(slug);
+  name.addEventListener('keyup', function () {
+    var nameAsSlug = this.value.replace(/\s+/g, '-').toLowerCase();
+    slug.value = nameAsSlug;
   });
 
-}(window.jQuery));
+}());
