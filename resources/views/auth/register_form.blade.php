3aae00a2435c0a64a80f1ed65d402cd990fdf9fd
Tobias <tobiaswhd@gmail.com>
2015-04-17 22:47:56 +0100

    Remove html comment block from register form

--- resources/views/auth/register_form.blade.php
+++ resources/views/auth/register_form.blade.php
@@ -1,12 +1,6 @@
 <form role="form" method="POST" action="{{ url('/auth/register') }}">
   <input type="hidden" name="_token" value="{{ csrf_token() }}">
-  <!-- <div class="form-group">
-    <label class="col-md-4 control-label">Name</label>
-    <div class="col-md-6">
-      <input type="text" class="form-control" name="name" value="{{ old('name') }}">
-    </div>
-  </div> -->
-
+  
   <div class="form-group">
     <label class="control-label">E-Mail Address</label>
       <input type="email" class="form-control" name="email" value="{{ old('email') }}">
