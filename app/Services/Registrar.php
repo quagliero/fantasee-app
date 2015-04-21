e25d0df7794f9a0850dc6f8f51f0c5669fe934fa
Tobias <tobiaswhd@gmail.com>
2015-04-21 21:59:49 +0100

    Remove check on name during registration, and hash pw on Model

--- app/Services/Registrar.php
+++ app/Services/Registrar.php
@@ -15,7 +15,6 @@ class Registrar implements RegistrarContract {
 	public function validator(array $data)
 	{
 		return Validator::make($data, [
-			'name' => 'required|max:255',
 			'email' => 'required|email|max:255|unique:users',
 			'password' => 'required|confirmed|min:6',
 		]);
@@ -29,10 +28,10 @@ class Registrar implements RegistrarContract {
 	 */
 	public function create(array $data)
 	{
+
 		return User::create([
-			'name' => $data['name'],
 			'email' => $data['email'],
-			'password' => bcrypt($data['password']),
+			'password' => $data['password'],
 		]);
 	}
 
