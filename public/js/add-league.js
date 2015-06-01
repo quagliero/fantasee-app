66bc89d34ca703b79329253cbde55a125da46a86
Tobias <tobiaswhd@gmail.com>
2015-06-01 22:22:32 +0100

    auto fill slug on blur, too

--- public/js/add-league.js
+++ public/js/add-league.js
@@ -4,9 +4,11 @@
   var name = document.getElementById('name');
   var slug = document.getElementById('slug');
 
-  name.addEventListener('keyup', function () {
-    var nameAsSlug = this.value.replace(/\s+/g, '-').toLowerCase();
+  function updateSlug() {
+    var nameAsSlug = name.value.replace(/\s+/g, '-').toLowerCase();
     slug.value = nameAsSlug;
-  });
-
+  }
+  
+  name.addEventListener('keyup', updateSlug);
+  name.addEventListener('blur', updateSlug);
 }());
