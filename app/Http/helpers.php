60c57eefd7e16b3ebcdc269176c4d6e1138e08c3
Matt Wheatley <terrawheat@gmail.com>
2015-06-18 19:13:12 +0000

    Native implementation of

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -110,11 +110,7 @@ function show_trophy($team)
 }
 
 function ordinal($number) {
-  $ends = array('th','st','nd','rd','th','th','th','th','th','th');
-  if ((($number % 100) >= 11) && (($number%100) <= 13)) {
-      return $number. 'th';
-    }
-  else {
-    return $number. $ends[$number % 10];
-  }
+  $fmt = new \NumberFormatter('en-US', \NumberFormatter::ORDINAL);
+
+  return $fmt->format( $number );
 }
