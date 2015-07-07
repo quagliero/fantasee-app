429d4674165ff0a275d97d06c39b646a49b54c6d
Tobias Harison-Denby <tobiaswhd@gmail.com>
2015-07-07 13:16:20 +0100

    Update authors!

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -2,8 +2,8 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-6 col-xs-12">
-        <p>Designed, built and maintained by <a href="https://twitter.com/quagliero">@quagliero</a><br>
-        <span style="font-size:.8em">League Commissioner of {!! link_to('http://chumbo.league.fantasy.nfl.com', 'The Chumbolone') !!}, and still upset about my 2012 Championship loss.</span><br>
+        <p>Designed, built and maintained by <a href="https://twitter.com/quagliero">@quagliero</a><sup>*</sup> &amp; <a href="https://twitter.com/mattdotdev">@mattdotdev</a><br>
+        <span style="font-size:.8em"><sup>*</sup>League Commissioner of {!! link_to('http://chumbo.league.fantasy.nfl.com', 'The Chumbolone') !!}, and still upset about my 2012 Championship loss.</span><br>
         <span style="font-size:.7em">Jacob Hester fullback dive, I mean come on. Seriously?</span></p>
       </div>
       <div class="col-sm-6 col-xs-12">
