02dd7151da5a03e7789d1315c57e9f6addcd1677
Tobias <tobiaswhd@gmail.com>
2015-07-07 19:38:24 +0100

    A little footer tweaking

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,12 +1,13 @@
 <footer class="site-footer">
   <div class="container">
     <div class="row">
-      <div class="col-sm-6 col-xs-12">
-        <p>Designed, built and maintained by <a href="https://twitter.com/quagliero">@quagliero</a><sup>*</sup> &amp; <a href="https://twitter.com/mattdotdev">@mattdotdev</a><br>
-        <span style="font-size:.8em"><sup>*</sup>League Commissioner of {!! link_to('http://chumbo.league.fantasy.nfl.com', 'The Chumbolone') !!}, and still upset about my 2012 Championship loss.</span><br>
-        <span style="font-size:.7em">Jacob Hester fullback dive, I mean come on. Seriously?</span></p>
+      <div class="col-sm-8 col-xs-12">
+        <p>Designed, built and maintained by <a href="https://twitter.com/quagliero">@quagliero</a><sup>*</sup> &amp; <a href="https://twitter.com/mattdotdev">@mattdotdev</a><sup>**</sup></p>
+        <p><span style="font-size:.8em"><sup>*</sup>League Commissioner of {!! link_to('http://chumbo.league.fantasy.nfl.com', 'The Chumbolone') !!}, and still upset about my 2012 Championship loss.</span><br>
+        <span style="font-size:.7em">Jacob Hester fullback dive, I mean come on. Seriously?</span><br>
+        <span style="font-size:.8em"><sup>**</sup> Go sports team!</span></p>
       </div>
-      <div class="col-sm-6 col-xs-12">
+      <div class="col-sm-4 col-xs-12">
         <p class="text-right">v.0.1.0</p>
       </div>
     </div>
