84596a98938f75eb9943649695401dbadb01af94
Tobias <tobiaswhd@gmail.com>
2016-04-20 12:23:24 +0100

    Hotfix: blade looping a ` as  => ` now seems to disregards order

--- resources/views/league_season/show.blade.php
+++ resources/views/league_season/show.blade.php
@@ -33,9 +33,9 @@
         <th></th>
       </thead>
       <tbody>
-      @foreach ($teams as $index => $team)
+      @foreach ($teams as $team)
         <tr>
-          <td>{!! $index + 1 !!}</td>
+          <td>{!! $team->position !!}</td>
           <td>{!! link_to_route('league_manager_season_path', $team->name, [$league->slug, $team->manager->id, $team->season->year]) !!}</td>
           <td>{!! link_to_route('league_manager_path', $team->manager->name, [$league->slug, $team->manager->id]) !!}</td>
           <td>{!! $team->wins !!}</td>
