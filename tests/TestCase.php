8dc39d8b98d6a5b911a5cfc3ba3f0b80dd2c0aa6
Tobias <tobiaswhd@gmail.com>
2016-04-20 16:09:50 +0100

    Upgrade to 5.2 requirement

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,30 +1,34 @@
 <?php
 
-class TestCase extends Illuminate\Foundation\Testing\TestCase {
+class TestCase extends Illuminate\Foundation\Testing\TestCase
+{
+    protected $baseUrl = 'http://localhost';
 
-	/**
-	 * Creates the application.
-	 *
-	 * @return \Illuminate\Foundation\Application
-	 */
-	public function createApplication()
-	{
-		$app = require __DIR__.'/../bootstrap/app.php';
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
 
-		$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
 
-		return $app;
-	}
+        return $app;
+    }
 
-  public function setUp() {
-    parent::setUp();
+    public function setUp()
+    {
+        parent::setUp();
 
-		$this->createApplication();
-    Artisan::call('migrate');
-  }
+        $this->createApplication();
+        Artisan::call('migrate');
+    }
 
-  public function tearDown() {
-    parent::tearDown();
-    Artisan::call('migrate:reset');
-  }
+    public function tearDown()
+    {
+        parent::tearDown();
+        Artisan::call('migrate:reset');
+    }
 }
