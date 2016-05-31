7de9f0b1d2cec9996fadd18ed640cbccacf567b4
Matt Wheatley <terrawheat@gmail.com>
2016-05-31 18:41:14 +0000

    Normalise exceptions toString so that tests pass in PHP5.6 and PHP7

--- app/Exceptions/DevOnlyException.php
+++ app/Exceptions/DevOnlyException.php
@@ -2,7 +2,11 @@
 
 class DevOnlyException extends \Exception {
   public function __construct($route, $code = 0, $previous = null) {
-    $message = "Unable to resolve $route as it is only intended for use in development environments.";
-    parent::__construct($message, $code, $previous);
+    $this->message = "Unable to resolve $route as it is only intended for use in development environments.";
+    parent::__construct($this->message, $code, $previous);
+  }
+
+  public function __toString() {
+    return self::class . ': ' . $this->message;
   }
 }
